c76597e4453eed11de4e3edae64c34cc786de4c8
Angelo <7332321+antjowie@users.noreply.github.com>
2020-09-13 15:36:26 +0200

    Added Noise Map generator

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise
+{
+    public static float[,] GenerateNoiseMap(int width, int height, float scale)
+    {
+        float[,] noiseMap = new float[width, height];
+
+        if(scale <= 0f)
+            scale = 0.0001f;
+
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                noiseMap[x,y] = Mathf.PerlinNoise(sampleX, sampleY);
+            }
+        }
+
+        return noiseMap;
+    }
+}
